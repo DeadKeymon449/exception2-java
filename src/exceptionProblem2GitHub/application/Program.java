61a1927114784d935ad1a93b384caa7c3c83bf1b
Matheus Tavares <mt937169@gmail.com>
2026-07-19 20:16:50 -0300

    Solution 1: Validation in Main

--- src/exceptionProblem2GitHub/application/Program.java
+++ src/exceptionProblem2GitHub/application/Program.java
@@ -1,5 +1,46 @@
 package exceptionProblem2GitHub.application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import exceptionProblem2GitHub.entities.Account;
+
 public class Program {
-	
+
+	public static void main(String[] arguments) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter account data\n");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		sc.nextLine();
+		System.out.print("Holder: ");
+		String holder = sc.nextLine();
+		System.out.print("Initial balance: ");
+		double balance = sc.nextDouble();
+		System.out.print("Withdraw limit: ");
+		double withdrawLimit = sc.nextDouble();
+
+		Account account = new Account(number, holder, balance, withdrawLimit);
+
+		System.out.println();
+		System.out.print("Enter amount for withdraw: ");
+
+		double amountToWithdraw = sc.nextDouble();
+
+		if (amountToWithdraw > account.getWithdrawLimit()) {
+			System.out.println("Withdraw error: The amount exceeds withdraw limit.");
+		} else {
+			if (amountToWithdraw > account.getBalance()) {
+				System.out.println("Withdraw error: Not enough balance.");
+			} else {
+				account.withdraw(amountToWithdraw);
+				System.out.println(account);
+			}
+		}
+
+		sc.close();
+	}
+
 }
---
+++ src/exceptionProblem2GitHub/entities/Account.java
@@ -0,0 +1,46 @@
+package exceptionProblem2GitHub.entities;
+
+public class Account {
+
+	private Integer number;
+	private String holder;
+	private Double balance;
+	private Double withdrawLimit;
+
+	public Account(Integer number, String holder, Double balance, Double withdrawLimit) {
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+		this.withdrawLimit = withdrawLimit;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public Double getWithdrawLimit() {
+		return withdrawLimit;
+	}
+
+	public void deposit(double amount) {
+		balance += amount;
+	}
+
+	public void withdraw(double amount) {
+		balance -= amount;
+	}
+
+	@Override
+	public String toString() {
+		return "New balance: " + String.format("%.2f", balance);
+	}
+	
+}
